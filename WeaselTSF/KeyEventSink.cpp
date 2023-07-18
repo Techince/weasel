fc83b60242a102225ab682963d473e001f39355f
Techince <fellow1984@sina.com>
2023-07-19 01:27:32 +0800

    Update KeyEventSink.cpp

--- WeaselTSF/KeyEventSink.cpp
+++ WeaselTSF/KeyEventSink.cpp
@@ -52,7 +52,6 @@ STDAPI WeaselTSF::OnTestKeyDown(ITfContext *pContext, WPARAM wParam, LPARAM lPar
 		return S_OK;
 	}
 	_ProcessKeyEvent(wParam, lParam, pfEaten);
-	_UpdateComposition(pContext);
 	if (*pfEaten)
 		_fTestKeyDownPending = TRUE;
 	return S_OK;
@@ -62,15 +61,15 @@ STDAPI WeaselTSF::OnKeyDown(ITfContext *pContext, WPARAM wParam, LPARAM lParam, 
 {
 	_fTestKeyUpPending = FALSE;
 	if (_fTestKeyDownPending)
-    {
+	{
 		_fTestKeyDownPending = FALSE;
 		*pfEaten = TRUE;
-    }
+	}
 	else
-    {
-		_ProcessKeyEvent(wParam, lParam, pfEaten);
-	    _UpdateComposition(pContext);
-    }
+	{
+		_ProcessKeyEvent(wParam, lParam, pfEaten);	    
+	}
+	_UpdateComposition(pContext);
 	return S_OK;
 } 
 
@@ -93,15 +92,15 @@ STDAPI WeaselTSF::OnKeyUp(ITfContext *pContext, WPARAM wParam, LPARAM lParam, BO
 {
 	_fTestKeyDownPending = FALSE;
 	if (_fTestKeyUpPending)
-    {
+	{
 		_fTestKeyUpPending = FALSE;
 		*pfEaten = TRUE;
-    }
+	}
 	else
-    {
+	{
 		_ProcessKeyEvent(wParam, lParam, pfEaten);
-        _UpdateComposition(pContext);
-    }
+		_UpdateComposition(pContext);
+	}
 	return S_OK;
 }
 
