6c0b82ddd8051c6d3e1ff6049f8bf06c0f5d9577
Techince <fellow1984@sina.com>
2023-07-19 01:41:15 +0800

    修复Word 2021搜索和替换第一编码无候选框#954

--- WeaselTSF/EditSession.cpp
+++ WeaselTSF/EditSession.cpp
@@ -35,14 +35,13 @@ STDAPI WeaselTSF::DoEditSession(TfEditCookie ec)
 		{
 			_EndComposition(_pEditSessionContext, true);
 		}
+		_UpdateUI(*context, _status);
 		_UpdateCompositionWindow(_pEditSessionContext);
 		if (_IsComposing() && config.inline_preedit)
 		{
 			_ShowInlinePreedit(_pEditSessionContext, context);
 		}
 	}
-
-	_UpdateUI(*context, _status);
 
 	return TRUE;
 }
